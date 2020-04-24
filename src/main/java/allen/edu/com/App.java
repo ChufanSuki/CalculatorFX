c8a810acfc574ceb6dbcf5022a11a1d3a3c08939
RetownPlato <allenplato28@gmail.com>
2020-04-25 00:20:26 +0800

    Initial commit

---
+++ src/main/java/allen/edu/com/App.java
@@ -0,0 +1,31 @@
+package allen.edu.com;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene = new Scene(root);
+//        scene.getStylesheets().add("org/kordamp/bootstrapfx/bootstrapfx.css");
+//        scene.getStylesheets().addAll(this.getClass().getResource("styles.css").toExternalForm());
+        stage.setTitle("Calculator");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/allen/edu/com/MainController.java
@@ -0,0 +1,165 @@
+package allen.edu.com;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Success");
+    }
+
+    private double data = 0.0;
+    private String operation = "AC";
+
+    private double readData(String input) {
+        if (input.indexOf(".") != input.length()-1) {
+            return Double.parseDouble(input);
+        }
+        return Double.parseDouble(input.substring(0,input.length()-2));
+    }
+
+    @FXML
+    private Button dot;
+
+    @FXML
+    private Button one;
+
+    @FXML
+    private Button two;
+
+    @FXML
+    private Button three;
+
+    @FXML
+    private Button four;
+
+    @FXML
+    private Button five;
+
+    @FXML
+    private Button six;
+
+    @FXML
+    private Button seven;
+
+    @FXML
+    private Button eight;
+
+    @FXML
+    private Button nine;
+
+    @FXML
+    private Button AC;
+
+    @FXML
+    private Button minus;
+
+    @FXML
+    private Button plus;
+
+    @FXML
+    private Button percent;
+
+    @FXML
+    private Button divide;
+
+    @FXML
+    private Button multiply;
+
+    @FXML
+    private Button invert;
+
+    @FXML
+    private Label output;
+
+    @FXML
+    private Button zero;
+
+    @FXML
+    private Button equal;
+
+    @FXML
+    public void handleButtonAction(ActionEvent event) {
+        if (operation.equals("Equal")) {
+            output.setText("");
+        }
+        if (event.getSource() == one) {
+            output.setText(output.getText()+"1");
+        } else if (event.getSource() == two) {
+            output.setText(output.getText()+"2");
+        } else if (event.getSource() == three) {
+            output.setText(output.getText()+"3");
+        } else if (event.getSource() == four) {
+            output.setText(output.getText()+"4");
+        } else if (event.getSource() == five) {
+            output.setText(output.getText()+"5");
+        } else if (event.getSource() == six) {
+            output.setText(output.getText()+"6");
+        } else if (event.getSource() == seven) {
+            output.setText(output.getText()+"7");
+        } else if (event.getSource() == eight) {
+            output.setText(output.getText()+"8");
+        } else if (event.getSource() == nine) {
+            output.setText(output.getText()+"9");
+        } else if (event.getSource() == zero) {
+            if (!output.getText().equals("0"))
+                output.setText(output.getText()+"0");
+        } else if (event.getSource() == AC) {
+            output.setText("");
+            operation = "AC";
+        } else if (event.getSource() == divide) {
+            operation = "Divide";
+            data = readData(output.getText());
+            output.setText("");
+        } else if (event.getSource() == multiply) {
+            operation = "Multiply";
+            data = readData(output.getText());
+            output.setText("");
+        } else if (event.getSource() == equal) {
+            double Secondhand = readData(output.getText());
+            switch (operation) {
+                case "Multiply":
+                    data *= Secondhand;
+                    break;
+                case "Minus":
+                    data -= Secondhand;
+                    break;
+                case "Divide":
+                    if (Secondhand == 0) throw new IllegalArgumentException("Argument 'divisor' is 0");
+                    data /= Secondhand;
+                    break;
+                case "Plus":
+                    data += Secondhand;
+                    break;
+            }
+            operation = "Equal";
+            output.setText(data+"");
+        } else if (event.getSource() == plus) {
+            operation = "Plus";
+            data = readData(output.getText());
+            output.setText("");
+        } else if (event.getSource() == minus) {
+            operation = "Minus";
+            data = readData(output.getText());
+            output.setText("");
+        } else if (event.getSource() == percent) {
+            operation = "Percent";
+            data = readData(output.getText());
+            data /= 100;
+            output.setText(data+"");
+        } else if (event.getSource() == dot) {
+            output.setText(data+".");
+        } else if (event.getSource() == invert) {
+            output.setText(""+output.getText());
+        }
+
+    }
+
+}
